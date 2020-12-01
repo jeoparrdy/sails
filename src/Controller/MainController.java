bca31aa70307a45a18a379f42359f6587ac037fb
jeoparrdy <jeoparrdy@gmail.com>
2020-12-01 16:52:39 +0200

    java.lang.IllegalStateException: Cannot forward after response has been committed was added!?

--- src/Controller/MainController.java
+++ src/Controller/MainController.java
@@ -1,9 +1,6 @@
 package Controller;
 
-import Model.Client;
-import Model.ClientDAO;
-import Model.Employer;
-import Model.EmployerDAO;
+import Model.*;
 
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
@@ -19,6 +16,9 @@ public class MainController extends HttpServlet {
     EmployerDAO employerDAO = new EmployerDAO();
     Client client = new Client();
     ClientDAO clientDAO = new ClientDAO();
+    Product product = new Product();
+    ProductDAO productDAO = new ProductDAO();
+
     int id;
 
     protected void processRequest(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
@@ -98,11 +98,16 @@ public class MainController extends HttpServlet {
                     client = clientDAO.search(number);
                     request.setAttribute("client", client);
                     break;
+                case "searchProduct":
+                    id = Integer.parseInt(request.getParameter("productCode"));
+                    product = productDAO.listId(id);
+                    request.setAttribute("product", product);
+                    break;
                 default:
-                    request.getRequestDispatcher("/saleRegister.jsp").forward(request, response);
+                    request.getRequestDispatcher("saleRegister.jsp").forward(request, response);
 //                    throw new AssertionError();
             }
-            request.getRequestDispatcher("/saleRegister.jsp").forward(request, response);
+            request.getRequestDispatcher("saleRegister.jsp").forward(request, response);
         }
 
 
